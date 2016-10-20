f2a3f5914150a56bbd1d8f09008ea0da1faf52af
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-10-20 11:32:33 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,11 +8,12 @@
 	date_default_timezone_set('Europe/Madrid');
 
 	// Globals
-	$installDate	= 'September 2016';										// Date Installed
-	$siteName		= 'MPTasks Demo';								// Site Name
+	$installDate	= 'September 2016';					// Date Installed
+	$siteName		= 'MPTasks Demo';				// Site Name
 	$siteUrl		= 'https://demo.miguelpiedrafita.com/tasks';	// Site URL, where you have uploaded Simple Tasks to. Include the trailing slash
-	$siteEmail		= 'soy@miguelpiedrafita.com';						// Site Email. Used in all email notifications
-	$language		= 'english';										// Site Language (options: english or custom). Language files are located in the "language" folder
+	$siteEmail		= 'soy@miguelpiedrafita.com';			// Site Email. Used in all email notifications
+	$language		= 'english';					// Site Language (options: english or custom). Language files are located in the "language" folder
+	$signupstatus		= true;							// Signup Status (options: true or false). Set it to false to disable signup.
 
-	define('PEPPER', 'LeRzpnVqmrDyZ$q6fZb#Sv2!7MqP3CPn');				// !IMPORTANT! Do NOT change this value once you have started using Simple Tasks.
+	define('PEPPER', 'LeRzpnVqmrDyZ$q6fZb#Sv2!7MqP3CPn');			// !IMPORTANT! Do NOT change this value once you have started using Tasks.
 ?>
